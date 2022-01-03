b02586e501b620727dcd4b149ef42601e0e00c50
dkochergin <dkochergin@luxoft.com>
2022-01-03 18:16:28 +0200

    ForKids application, add button tap sound

--- frontend/src/components/Dashboard/ForKids/ForKids.tsx
+++ frontend/src/components/Dashboard/ForKids/ForKids.tsx
@@ -21,6 +21,8 @@ const descriptions = [
 const ForKids: FC = (): JSX.Element => {
 
   let reactSwipeEl: ReactSwipe;
+  // preload audio
+  new Audio(process.env.PUBLIC_URL + '/audio/ES_Arcade Game Tap 1.mp3');
 
   return (
     <div className='p-10'>
@@ -31,7 +33,7 @@ const ForKids: FC = (): JSX.Element => {
           ref={(el: ReactSwipe) => (reactSwipeEl = el)}
         >
           {descriptions.map((description) => {
-            return (<div>
+            return (<div key={description[1]}>
               <div className={'item flex flex-col'}>
                 <div className={'description'}>
                   {description[1]}
@@ -42,8 +44,20 @@ const ForKids: FC = (): JSX.Element => {
           })}
         </ReactSwipe>
         <div className={'carousel-buttons flex flex-row justify-between'}>
-          <div className={'carousel-button'} onClick={() => reactSwipeEl.prev()}> &lt;&lt; </div>
-          <div className={'carousel-button'} onClick={() => reactSwipeEl.next()}> &gt;&gt; </div>
+          <div className={'carousel-button'} onClick={() => {
+            reactSwipeEl.prev();
+            const audio = new Audio(process.env.PUBLIC_URL + '/audio/ES_Arcade Game Tap 1.mp3');
+            audio.addEventListener('canplaythrough', event => {
+              void audio.play();
+            });
+          }}> &lt;&lt; </div>
+          <div className={'carousel-button'} onClick={() => {
+            reactSwipeEl.next();
+            const audio = new Audio(process.env.PUBLIC_URL + '/audio/ES_Arcade Game Tap 1.mp3');
+            audio.addEventListener('canplaythrough', event => {
+              void audio.play();
+            });
+          }}> &gt;&gt; </div>
         </div>
       </div>
     </div>
